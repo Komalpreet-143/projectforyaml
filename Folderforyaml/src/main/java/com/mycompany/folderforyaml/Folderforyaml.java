17b658edfaa0df888c752e20858b6092d3ea54bf
Komalpreet-143 <kau17089@sheridancollege.ca>
2026-02-04 15:13:09 -0500

    LuckyCard included

---
+++ Folderforyaml/src/main/java/com/mycompany/folderforyaml/Folderforyaml.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.folderforyaml;
+
+/**
+ *
+ * @author komal
+ */
+public class Folderforyaml {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
